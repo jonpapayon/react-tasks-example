10aa0f9b853d0173f28f22eea57894e02409b811
jonpapayon <whyjonpapayon@gmail.com>
2022-12-09 10:21:43 -0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from "react"
+import TaskList from './components/TaskList'
+import TaskForm from "./components/TaskForm"
+
+function App() {
+  return (
+    <main className="bg-zinc-900 h-screen">
+      <div className="container mx-auto p-12">
+        <TaskForm/>
+        <TaskList/>
+      </div>
+    </main>
+  )
+}
+
+export default App
